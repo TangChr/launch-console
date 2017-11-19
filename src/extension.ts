7bfca9c2be1778a0f80f2279d145888af5b5dfa0
Christian Tang <git@tcdev.dk>
2017-11-19 04:29:31 +0100

    Added "Open shell" to statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,20 @@
-import {window, commands, workspace, ExtensionContext, StatusBarItem, StatusBarAlignment} from 'vscode'; // Import VS Code
+import {window, commands, workspace, StatusBarItem, StatusBarAlignment} from 'vscode'; // Import VS Code
 import * as child_process from 'child_process';
+var statusItem: StatusBarItem;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate() {
-	// Register Commands
+     
+    if (!statusItem)
+    {
+        let config = workspace.getConfiguration("openShell");
+        let statusBarText = config.get("text") as string;
+
+        statusItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        statusItem.text = statusBarText;
+        statusItem.command = 'extension.openShell';
+        statusItem.show();
+    }
+
 	commands.registerCommand('extension.openShell', () => openShell());
 }
 
@@ -12,15 +22,22 @@ function openShell()
 {
     let config = workspace.getConfiguration("openShell");
     let shellPath = config.get("shell") as string;
-    let rootPath = workspace.rootPath != null ? workspace.rootPath : "C:\\";
-    //let rootPath = "E:\\GitHub\\open-shell";
+    let defaultDir = config.get("defaultDir") as string;
+    let rootPath = workspace.rootPath != null ? workspace.rootPath : defaultDir;
 
     let command = formatCommand(shellPath, rootPath);
     child_process.exec(command);
 }
 
+export function deactivate() {
+
+    // Clean up
+    if (statusItem) {
+        statusItem.dispose();
+        statusItem = null;
+    }
+}
+
 function formatCommand(shellPath: string, rootPath: string): string {
-    return shellPath
-        .replace('%path%', rootPath)
-        .replace('%description%', 'ConsoleZ');
+    return shellPath.replace('%path%', rootPath);
 }
